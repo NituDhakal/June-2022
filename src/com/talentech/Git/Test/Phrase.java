0668d969c324c3c76e9e9799760b2dac38972a04
Nitu <nitusdk@gmail.com>
2022-06-12 20:15:32 -0400

    My first commit for 2nd Git Project.

---
+++ src/com/talentech/Git/Test/Phrase.java
@@ -0,0 +1,13 @@
+package com.talentech.Git.Test;
+
+public class Phrase {
+
+	public static void main(String[] args) {
+		Phrase phrase = new Phrase();
+		phrase.phrase();
+	}
+
+	public void phrase() {
+		System.out.println("We are learning Java.");
+	}
+}
